981123cc3f74f2bb3b42d982751dc63c227cd15c
mnstrong <marynstrong@gmail.com>
2026-01-21 11:57:32 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Google Calendar API Configuration
+// Keep this file private and don't share it publicly
+
+const CONFIG = {
+    GOOGLE_CLIENT_ID: '711113551828-7ttas1jf0nke59msbhii5gpc7us06reu.apps.googleusercontent.com',
+};
