d261152211fe47da2dba07d60f0ec3c2674b5d87
mnstrong <marynstrong@gmail.com>
2026-02-28 20:18:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3361,7 +3361,8 @@ let visiblePeriods = {
             var tint = tintColor ? hexToRgba(tintColor, 0.55) : 'rgba(0,0,0,0.45)';
             el.style.backgroundImage = 'linear-gradient(' + tint + ', ' + tint + '), url(' + imgUrl + ')';
             el.style.backgroundSize = 'cover';
-            el.style.backgroundPosition = 'center';
+            el.style.backgroundPosition = 'center center';
+            el.style.backgroundOrigin = 'border-box';
             el.querySelectorAll('.sg-event-title, .sg-event-time, .day-view-event-title, .day-view-event-time, .day-view-event-member').forEach(function(t) {
                 t.style.color = '#fff';
             });
