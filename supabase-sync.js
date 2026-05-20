67e7e8b9aa5eec2598817b636e1d94e71a200c9e
mnstrong <marynstrong@gmail.com>
2026-05-19 21:52:59 -0700

    Add countdown property to event handling

--- supabase-sync.js
+++ supabase-sync.js
@@ -247,6 +247,7 @@ async function loadAllDataFromSupabase() {
                     time: e.all_day ? '' : (startDt ? pad(startDt.getHours()) + ':' + pad(startDt.getMinutes()) : ''),
                     endTime: e.all_day ? '' : (endDt ? pad(endDt.getHours()) + ':' + pad(endDt.getMinutes()) : ''),
                     isAllDay: e.all_day || false,
+                    countdown: e.countdown || false,
                     member: memberInfo ? memberInfo.name : '',
                     members: memberInfo ? [memberInfo.name] : [],
                     googleId: e.google_event_id || null,
@@ -673,7 +674,8 @@ async function syncCalendarEvent(event, operation) {
             end_time:         endTime,
             all_day:          event.isAllDay ? true : false,
             family_member_id: familyMemberId,
-            google_event_id:  event.googleId || null
+            google_event_id:  event.googleId || null,
+            countdown:        event.countdown ? true : false
         };
 
         if (operation === 'add') {
@@ -1034,6 +1036,7 @@ function startPeriodicRefresh() {
                         time: e.all_day ? '' : (startDt ? pad(startDt.getHours()) + ':' + pad(startDt.getMinutes()) : ''),
                         endTime: e.all_day ? '' : (endDt ? pad(endDt.getHours()) + ':' + pad(endDt.getMinutes()) : ''),
                         isAllDay: e.all_day || false,
+                        countdown: e.countdown || false,
                         member: memberInfo ? memberInfo.name : '',
                         members: memberInfo ? [memberInfo.name] : [],
                         googleId: e.google_event_id || null,
